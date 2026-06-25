7c578023e0e29b74b3681c3f1c65b6dcd01c351a
George Gomes <georgecarlos@live.com>
2026-06-25 21:54:00 +0100

    ajustes na classe user
    retirados metodos com input, pois a classe recebe isto por parametro e nao por input direto.

--- user/user.py
+++ user/user.py
@@ -1,35 +1,19 @@
 from error_treatment import error
 
-class USER:
-    def __init__(self):
-        self.first_name = None
-        self.last_name = None
-        self.mail = None
-        
-    def set_first_name(self):
-        """ Set the users first name"""
-        try:
-            self.first_name = input("First name: ").lower()
-        except Exception as e:
-            error.error_log_message(e)
-    def set_last_name(self):
-        """ Set the users last name """
-        try:
-            self.last_name = input("Last name: ").lower()
-        except Exception as e:
-            error.error_log_message(e)
+class User:
+    def __init__(self, first_name, last_name, user_mail):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_mail = user_mail
+
     def get_user_full_name(self):
         try:
             return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
         except Exception as e:
             error.error_log_message(e)
-    def set_user_mail(self):
-        try:
-            self.mail = input("Digite seu e-mail: ").lower()
-        except Exception as e:
-            error.error_log_message(e)
+
     def get_user_mail(self):
         try:
-            return self.mail if self.mail else "Não há e-mail registado."
+            return self.user_mail if self.user_mail else "Não há e-mail registado."
         except Exception as e:
             error.error_log_message(e)
