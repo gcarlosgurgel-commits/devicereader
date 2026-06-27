b44b366fc8b2f7f05e2eecc8a45a255a2f13ba7c
George Gomes <georgecarlos@live.com>
2026-06-27 02:28:32 +0100

    adicionado sqlachemy

---
+++ database/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite:///./devicereader.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from database.database import engine, SessionLocal
+from database import models
+
 
 from user.user import User
 from hardware.cpu import CPU
@@ -10,17 +14,37 @@ from models import UserRequest
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def root():
     return {"message": "DeviceReader API funcionando"}
 
 @app.post("/users")
-def create_user(data: UserRequest):
-    user = User(data.first_name, data.last_name, data.user_mail)
-    return{
-        "full_name": user.get_user_full_name(),
-        "last_name": user.get_user_mail()
+def create_user(data: UserRequest, db: Session = Depends(get_db)):
+
+    user = models.User(
+        first_name=data.first_name,
+        last_name=data.last_name,
+        user_mail=data.user_mail
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    
+    return {
+        "id": user.id,
+        "full_name": f"{user.first_name.capitalize()} {user.last_name.capitalize()}",
+        "user_mail": user.user_mail,
+        "created_at": user.created_at
     }
 
 @app.get("/hardware")
