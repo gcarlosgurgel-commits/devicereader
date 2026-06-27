9e7743826f6a63e5a24b820cbc3e875f5af87b36
George Gomes <georgecarlos@live.com>
2026-06-27 02:58:20 +0100

    uptodate

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = "sqlite:///./devicereader.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False, "echo": True})
 
 SessionLocal = sessionmaker(bind=engine)
 
