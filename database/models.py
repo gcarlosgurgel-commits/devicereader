b6ad5c1d296c6b2015582ee079b0b01737a1b5ab
George Gomes <georgecarlos@live.com>
2026-06-27 03:48:45 +0100

    uptodate

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, MappedColumn
 from sqlalchemy.sql import func
 from database.database import Base
 
@@ -7,11 +7,11 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    user_mail = Column(String, unique=True)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    first_name: Mapped[str] = Column(String)
+    last_name: Mapped[str] = Column(String)
+    user_mail: Mapped[str] = Column(String, unique=True)
+    created_at: Mapped[DateTime] = Column(DateTime, server_default=func.now())
+    updated_at: Mapped[DateTime] = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
     devices = relationship("Device", back_populates="user")
 
